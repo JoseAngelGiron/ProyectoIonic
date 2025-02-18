72b9ba35fed3f8fe880963f5dbfb64a6019d40ac
JoseAngel <Jangelgiron@gmail.com>
2025-02-18 20:14:55 +0100

    Realizados cambios para el login

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import {
-  IonContent
-} from '@ionic/angular/standalone';
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { IonContent, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
-import {LoginComponent} from "../../components/login/login.component";
+import { CustomInputComponent } from 'src/app/shared/components/custom-input/custom-input.component';
+import { addIcons } from 'ionicons';
+import {
+  lockClosedOutline,
+  mailOutline,
+  personAddOutline,
+  logInOutline,
+} from 'ionicons/icons';
+import { IonButton } from '@ionic/angular/standalone';
+import { LogoComponent } from 'src/app/shared/components/logo/logo.component';
+import { RouterLink } from '@angular/router';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-auth',
@@ -13,15 +29,41 @@ import {LoginComponent} from "../../components/login/login.component";
   styleUrls: ['./auth.page.scss'],
   standalone: true,
   imports: [
+    IonIcon,
     HeaderComponent,
     IonContent,
     CommonModule,
     FormsModule,
-    LoginComponent,
+    CustomInputComponent,
+    ReactiveFormsModule,
+    IonButton,
+    LogoComponent,
+    RouterLink,
   ],
 })
 export class AuthPage implements OnInit {
-  constructor() {}
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  firebaseService = inject(FirebaseService);
+
+
+  constructor() {
+    addIcons({
+      mailOutline,
+      lockClosedOutline,
+      personAddOutline,
+      logInOutline,
+    });
+  }
 
   ngOnInit() {}
+
+  submit() {
+    this.firebaseService.signIn(this.form.value as User).then(res =>
+      console.log(res)
+    );
+  }
 }
